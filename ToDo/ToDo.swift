16614942952b569f99e5e4ccc20521766fe1e747
Sydney Tam <tam.sydney@gmail.com>
2022-08-02 10:22:40 -0700

    Initial Commit

---
+++ ToDo/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDo
+//
+//  Created by Sydney Tam on 8/2/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+    
+}
---
+++ ToDo/addToDoTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  addToDoTableViewController.swift
+//  ToDo
+//
+//  Created by Sydney Tam on 8/2/22.
+//
+
+import UIKit
+
+class addToDoTableViewController: UITableViewController {
+
+    
+    var toDos : [ToDo] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the dog"
+        
+        
+        return [swift, dog]
+    }
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let toDo =
+            toDos[indexPath.row]
+        
+        
+        if toDo.important {
+            cell.textLabel?.text = " ❗️ " + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+
+        return cell
+        
+    }
+    
+
+   
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
